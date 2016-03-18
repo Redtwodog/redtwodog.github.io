b8ecf1f13e45c5c1415d3d2ce7a95a0ddd6e4e7e
Redtwodog <646945267@qq.com>
2016-03-18 16:16:44 +0800

    引入动画框架

--- js/menu.js
+++ js/menu.js
@@ -1,48 +1,8 @@
 window.onload = function() {
 	var close = document.getElementById('menu');
 	close.onmouseover = function() {
-		startMove(0);
-		startOpactiy(100);
+		startMove(close,{left:0,opacity:100})
 	};
 	close.onmouseout = function() {
-		startMove(-200);
-		startOpactiy(30);
-	}
-}
-var timer = null;
-var timer2 = null;
-var alpha = 30;
-
-function startMove(ev) {
-	var close = document.getElementById('menu');
-	clearInterval(timer);
-	var speed = (ev - close.offsetLeft) / 40;
-	speed=speed>0?Math.ceil(speed):Math.floor(speed);
-	timer = setInterval(function() {
-		if (close.offsetLeft == ev) {
-			clearInterval(timer)
-		} else {
-			close.style.left = menu.offsetLeft + speed + 'px'
-		}
-	}, 30)
-}
-
-function startOpactiy(target) {
-	var close = document.getElementById('menu');
-	clearInterval(timer2);
-	var speed2 = 0;
-	timer2 = setInterval(function() {
-		if (alpha > target) {
-			speed2 = -10;
-		} else {
-			speed2 = 10;
-		}
-		if (alpha == target) {
-			clearInterval(timer2)
-		} else {
-			alpha += speed2;
-			close.style.filter = 'alpha(opacity=' + alpha + ')';
-			close.style.opacity = alpha / 100;
-		}
-	}, 30)
-}
+		startMove(close,{left:-200,opacity:30});
+	}}
