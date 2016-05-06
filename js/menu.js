1db2d8ac3d0935f8f5630b53141085a52c8ee768
Redtwodog <646945267@qq.com>
2016-05-06 12:48:38 +0800

    5-6
    hexo

--- js/menu.js
+++
@@ -1,8 +0,0 @@
-window.onload = function() {
-	var close = document.getElementById('menu');
-	close.onmouseover = function() {
-		startMove(close,{left:0,opacity:100})
-	};
-	close.onmouseout = function() {
-		startMove(close,{left:-200,opacity:30});
-	}}
